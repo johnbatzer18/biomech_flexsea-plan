25c04d793fa40f41ab77060080073184225faace
Jean-Francois Duval <jfduval@aqra.ca>
2017-10-13 09:25:19 -0400

    Added a Dark color theme. WiP

--- src/main.cpp
+++ src/main.cpp
@@ -39,6 +39,8 @@
 #include "mainwindow.h"
 #include <QApplication>
 
+#define USE_DARK_THEME
+
 //****************************************************************************
 // Main
 //****************************************************************************
@@ -73,6 +75,36 @@ int main(int argc, char *argv[])
 		return 0;
 
 	#else
+
+		#ifdef USE_DARK_THEME
+		qApp->setStyle(QStyleFactory::create("Fusion"));
+
+		QPalette darkPalette;
+		darkPalette.setColor(QPalette::Window, QColor(53,53,53));
+		darkPalette.setColor(QPalette::WindowText, Qt::white);
+		darkPalette.setColor(QPalette::Base, QColor(25,25,25));
+		darkPalette.setColor(QPalette::AlternateBase, QColor(53,53,53));
+		darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
+		darkPalette.setColor(QPalette::ToolTipText, Qt::white);
+		darkPalette.setColor(QPalette::Text, Qt::white);
+		darkPalette.setColor(QPalette::Button, QColor(53,53,53));
+		darkPalette.setColor(QPalette::ButtonText, Qt::white);
+		darkPalette.setColor(QPalette::BrightText, Qt::red);
+
+
+		//darkPalette.setColor(QPalette::Link, QColor(42, 130, 218));
+		darkPalette.setColor(QPalette::Link, QColor(204, 86, 0));
+
+		//Window selected bar
+		//darkPalette.setColor(QPalette::Highlight, QColor(42, 130, 218));
+		darkPalette.setColor(QPalette::Highlight, QColor(204, 86, 0));
+
+		darkPalette.setColor(QPalette::HighlightedText, Qt::black);
+
+		qApp->setPalette(darkPalette);
+
+		qApp->setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }");
+		#endif //USE_DARK_THEME
 
 		initLocalComm();
 		initializeGlobalStructs();
